4d25549dcdfa0fda840fb777289ffb27bb0c97d1
Olaitan.A <52823454+moa-mel@users.noreply.github.com>
2020-06-06 01:23:33 +0100

    dele
    deleted

--- mel_script.js
+++
@@ -1,7 +0,0 @@
-const name = "Olaitan Akano";
-const id = "HNG-06138";
-const language = "javascript";
-const email = "olaitanmaryakano@gmail.com";
-const result = 'Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task ${email}';
-console.log(result);
-
