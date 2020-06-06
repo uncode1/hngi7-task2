8961afabecb1b10e39ccd713db1cb56621f047b8
uncode1 <53866169+uncode1@users.noreply.github.com>
2020-06-06 01:59:46 +0100

    Create HNG-05479.py

---
+++ HNG-05479.py
@@ -0,0 +1,6 @@
+Name = "SUMANU, Peter"
+hngi7_id = "HNG-05479"
+Language = "Python"
+Email = "petersumanu@yahoo.com"
+
+print("Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task email {}.".format (Name, hngi7_id, Language, Email))
